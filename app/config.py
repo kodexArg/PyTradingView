483ef5be1115ca8377d25a08737c073f6513c466
Gabriel Alejandro Cavedal <47777332+kodexArg@users.noreply.github.com>
2023-08-31 17:01:22 -0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
-bot_token = '1901938056:AAFZZGLX_73DCI8R29SFolqBR078bf4p1jA'
+bot_token = 'asdfasdfasdf'
 #bot_chatid = '780726530'
 
 bot_chatid = '-546860246'
 
-api_key = "S1q86jqksP5M7DxYZZTQFYqCbhqfa7CpbJVHY3783bp2jXxCmUw97GhmJ9TtE10L"
-api_sec = "4WavDD23aJ4MarGh7iyYcL0vrA3wHHJiJrvfUDWh7vD9Onl1d3vcA2UjXBfV2ebF"
+api_key = "asdfasdfasdfasdfasdf"
+api_sec = "asdfasdfasdf"
